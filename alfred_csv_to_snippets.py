2a87543a8acbb3bbc74c94b1651e52441c56d0ea
Michael Grossklos <foofourtyone@gmail.com>
2020-10-12 07:33:09 +0200

    adding env

--- alfred_csv_to_snippets.py
+++ alfred_csv_to_snippets.py
@@ -24,89 +24,94 @@
 # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import logging
+import re
+import shutil
+import subprocess
+import time
+import uuid
 from csv import DictReader
 from json import dumps
-from os import urandom, path, mkdir, walk
+from os import path, mkdir, walk
 from sys import exit
 
-import subprocess
-import uuid
-import logging
-import shutil
-import time
-import re
-
 logging.basicConfig(level=logging.DEBUG)
+
 
 def dd(msg):
     logging.debug(msg)
     exit()
 
+
 def errorMsg(message):
     print('ERROR: ' + message)
-    exit()  
+    exit()
+
 
 def replaceEmbededSnippets(content, sc_left, sc_right, rc_left, rc_right):
     content = re.sub(rf'{sc_left}(\w+\:\w+){sc_right}', rf'{rc_left}\1{rc_right}', content)
     content = re.sub(rf'{sc_left}\|', '{cursor}', content)
     return content
-    
+
 
 print("Specify the path where the CSV files are located:")
 inputPath = input()
 if not path.isdir(inputPath):
-        errorMsg("The path you provided does not exist or isn't a directory:\n" + inputPath) 
+    errorMsg("The path you provided does not exist or isn't a directory:\n" + inputPath)
 
 print("Is this the order in which the content in your csv files is sorted: 'keyword, content, name'? (Y/n):")
 isSorted = input()
-if isSorted == "y" or isSorted =="":
+if isSorted == "y" or isSorted == "":
     fieldNames = ["keyword", "content", "name"]
 else:
     print("Specify the order in which the content is sorted (f.e.: 'name, content, keyword'):")
-    inputSorting = input()  
+    inputSorting = input()
     fieldNames = inputSorting.replace(",", " ").replace("'", "").split()
     if type(fieldNames) != list:
-        errorMsg("Your provided order is not a list like: 'keyword, content, name'")  
+        errorMsg("Your provided order is not a list like: 'keyword, content, name'")
 
 print("Where to save the converted files?")
 targetPath = input()
 if not path.isdir(targetPath):
-        errorMsg("The path you provided does not exist or isn't a directory:\n" + targetPath) 
+    errorMsg("The path you provided does not exist or isn't a directory:\n" + targetPath)
 
-print("In order to convert the csv files into *.alfredsnippets files, this script generates one subfolder per file into which the single snippet files get saved. Do you want to keep those folders, otherwise they get deleted? (y/N)")
+print(
+    "In order to convert the csv files into *.alfredsnippets files, this script generates one subfolder per file into "
+    "which the single snippet files get saved. Do you want to keep those folders, otherwise they get deleted? (y/N)")
 keepFolders = input()
 
 createdFolders = []
 
 for root, dirs, files in walk(inputPath):
     for outputFile in files:
-        if outputFile.endswith('.csv'): 
+        if outputFile.endswith('.csv'):
 
             fileName, fileExtension = path.splitext(outputFile)
-            outputPath=""
+            outputPath = ""
             outputPath = targetPath + "/" + fileName
 
             try:
                 mkdir(outputPath)
                 print(f'Making folder {outputPath}...')
             except OSError:
-                print (f"Creation of the directory {path} failed")
+                print(f"Creation of the directory {path} failed")
 
-
-            with open (path.join(inputPath, outputFile), 'rt') as csvfile:
+            with open(path.join(inputPath, outputFile), 'rt') as csvfile:
                 reader = DictReader(csvfile, fieldnames=fieldNames)
-                
-                for row in reader:    
+
+                for row in reader:
                     uid = str(uuid.uuid1()).upper()
                     row['content'] = replaceEmbededSnippets(row['content'], '%', '%', '{', '}')
                     logging.debug(row['content'])
-                    output = dumps({"alfredsnippet" : {"snippet" : row['content'], "uid": uid, "name" : row['name'], "keyword" : row['keyword']}}, sort_keys=False, indent=4, separators=(',', ': '))
-                    outputFile = row['name']+" ["+uid+"].json"
-                    target = outputPath + "/" + outputFile              
+                    output = dumps({"alfredsnippet": {"snippet": row['content'], "uid": uid, "name": row['name'],
+                                                      "keyword": row['keyword']}}, sort_keys=False, indent=4,
+                                   separators=(',', ': '))
+                    outputFile = row['name'] + " [" + uid + "].json"
+                    target = outputPath + "/" + outputFile
                     f = open(target, 'w')
                     f.write(output)
                     f.close()
-                    print(f"Writing file {target}...")    
+                    print(f"Writing file {target}...")
         else:
             errorMsg("The files in the provided folder are not *.csv files.")
 
@@ -119,8 +124,3 @@ if keepFolders == "n" or keepFolders == "":
     for f in createdFolders:
         shutil.rmtree(targetPath + "/" + f)
         print(f"{targetPath}/{f} was deleted")
-
-
-
-
-
